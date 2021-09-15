3abcfc2a5620507164b81a88d06d7335430ea7a8
Athul Krishnan P M <athul.pm@pelatro.com>
2021-09-15 22:10:11 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 const routes: Routes = [
+{path: '', component: LoginComponent},
+{path: 'Product',component:ShoppingCartComponent},
 {path: 'login', component: LoginComponent},
 ];
 @NgModule({
